4c6fefea73c0b44875c7b8901ee1715a88358367
root <root@hiiu>
2025-04-11 03:30:35 +0700

    ok

--- btc_scanFullAddr/btc_scanFullAddr/Main.cpp
+++ btc_scanFullAddr/btc_scanFullAddr/Main.cpp
@@ -1,5 +1,6 @@
 // // MAXX.SetBase10("115792089237316195423570985008687907852837564279074904382605163141518161494336"); // full
-// MAXX.SetBase10("11579208923731619542357098500868790785283756427907490438260516314151"); // remove 10th-end
+// MAXX.SetBase10("115792089237316195423570985008687907852837564279074904382605163141518"); // remove 9th-end - _1B
+// MAXX.SetBase10("11579208923731619542357098500868790785283756427907490438260516314151"); // remove 10th-end - _10B
 
 #include "Timer.h"
 #include "KeyHunt.h"
@@ -69,7 +70,8 @@ void init_value(int mode, uint64_t xN,Int& privDec, Int& rangeStart, Int& rangeE
     Int MINN, MAXX;
     MINN.SetBase10("0");
     // MAXX.SetBase16("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364140"); // full
-    MAXX.SetBase10("11579208923731619542357098500868790785283756427907490438260516314151"); // remove 10th-end
+    MAXX.SetBase10("115792089237316195423570985008687907852837564279074904382605163141518"); // remove 9th-end - _1B
+    // MAXX.SetBase10("11579208923731619542357098500868790785283756427907490438260516314151"); // remove 10th-end - _10B
     // std::cout << std::endl << "MAXX-10 : " << MAXX.GetBase10();
     // std::cout << std::endl << "MAXX-16 : " <<  MAXX.GetBase16();
 
@@ -99,23 +101,23 @@ void init_value(int mode, uint64_t xN,Int& privDec, Int& rangeStart, Int& rangeE
             // break;
         }
 
-        // set _10B
-        Int _10B, _xN10B;
-        _10B.SetBase10("10000000000"); 
-        // set xN10B = _10B then multiple to xN
-        _xN10B = _10B;
-        _xN10B.Mult(xN); 
+        // set _1B
+        Int _1B, _xN1B;
+        _1B.SetBase10("1000000000"); 
+        // set xN1B = _1B then multiple to xN
+        _xN1B = _1B;
+        _xN1B.Mult(xN); 
 
 
         //set --- rangeStart
         rangeStart = privDec; 
-        rangeStart.Mult(&_10B);
+        rangeStart.Mult(&_1B);
         // std::cout << std::endl << "rangeStart-10 :  " << rangeStart.GetBase10();
         // std::cout << std::endl << "rangeStart-16 :  " << rangeStart.GetBase16() << std::endl;
 
         //set --- rangEnd
         rangeEnd = rangeStart;
-        rangeEnd.Add(&_xN10B);
+        rangeEnd.Add(&_xN1B);
         // std::cout << std::endl << "rangeEnd-10 :    " << rangeEnd.GetBase10();
         // std::cout << std::endl << "rangeEnd-16 :    " << rangeEnd.GetBase16() << std::endl;
 
